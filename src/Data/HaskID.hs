11beeb70e3fb0105166e19de3f34186c58ab6d12
bryant <bryant@defrag.in>
2014-12-10 00:25:53 -0500

    encoding without length consideration

--- src/Data/HaskID.hs
+++ src/Data/HaskID.hs
@@ -1,7 +1,30 @@
 module Data.HaskID where
 
 import Data.Char (ord)
-import Data.List (foldl', zipWith4)
+import Data.List (foldl', zipWith4, mapAccumL)
+import Numeric (showIntAtBase)
+
+alp :: String
+alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+encode_raw :: [Int] -> String -> String -> String -> String
+encode_raw input salt alpha separators = encoded' `interleave_with` seps
+    where
+    seps = mk_seps separators input encoded'
+    encoded' = [seed] : map_accum (enc_step seedsalt) alpha input
+    seedsalt = seed : salt
+    seed = alpha !!% sum (zipWith rem input [100..])
+
+mk_seps :: String -> [Int] -> [String] -> String
+mk_seps seps input chunks = zipWith3 mk' [0..] input chunks
+    where mk' idx val c = seps !!% (val `rem` (ord (head c) + idx))
+
+enc_step :: String -> String -> Int -> (String, String)
+enc_step salt alpha val = (alpha', last)
+    where
+    alpha_salt = take (length alpha) (salt ++ alpha)
+    alpha' = shuffle alpha alpha_salt
+    last = showIntAtBase (length alpha') (alpha' !!) val ""
 
 shuffle :: String -> String -> String
 shuffle xs = foldl' swap xs . mk_swap_points (length xs)
@@ -23,3 +46,14 @@ swap xs (i, j)
 
 seg :: [a] -> Int -> Int -> [a]
 seg xs start end = take (end - start) (drop start xs)
+
+(!!%) :: [a] -> Int -> a
+xs !!% n = xs !! (n `rem` length xs)
+
+map_accum :: (a -> b -> (a, c)) -> a -> [b] -> [c]
+map_accum f accum = snd . mapAccumL f accum
+
+interleave_with :: [[a]] -> [a] -> [a]
+interleave_with [] _ = []
+interleave_with chunks seps = c ++ concat (zipWith (:) seps hunks)
+    where (c, hunks) = (head chunks, tail chunks)
