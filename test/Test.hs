5f72e952a24f1c709c1a8c2d4734ec887c8b4ede
bryant <bryant@defrag.in>
2014-12-11 13:28:03 -0500

    simpler

--- test/Test.hs
+++ test/Test.hs
@@ -5,4 +5,4 @@ import Data.HaskID (encode, default_settings, decode)
 
 round_trip_prop :: [Positive Int] -> Bool
 round_trip_prop xs = decode default_settings (encode default_settings xs') == xs'
-    where xs' = map (\(Positive x) -> x) xs
+    where xs' = map getPositive xs
