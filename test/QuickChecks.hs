69f8f8cc8e6430566ca24b7dec767e02387eaf6a
bryant <bryant@defrag.in>
2014-12-15 16:04:37 -0500

    import from tasty

--- test/QuickChecks.hs
+++ test/QuickChecks.hs
@@ -1,8 +1,12 @@
 module QuickChecks where
 
-import Test.QuickCheck (quickCheck, forAll, listOf, arbitrary, Positive(..))
+import Test.Tasty.QuickCheck (forAll, listOf, arbitrary, Positive(..),
+                              testProperty)
+import Test.Tasty (testGroup)
 import Data.HaskID (encode, default_settings, decode)
 
 round_trip_prop :: [Positive Int] -> Bool
 round_trip_prop xs = decode default_settings (encode default_settings xs') == xs'
     where xs' = map getPositive xs
+
+qchecks = testGroup "Properties" [testProperty "round trip" round_trip_prop]
