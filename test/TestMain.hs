a402c7cd79a0ad39322c955d1b2bd614bee056b4
bryant <bryant@defrag.in>
2014-12-15 16:04:52 -0500

    add main entry point for tests

---
+++ test/TestMain.hs
@@ -0,0 +1,8 @@
+module TestMain where
+
+import Test.Tasty (defaultMain, testGroup)
+import QuickChecks (qchecks)
+import Cases (encode_cases)
+
+main :: IO ()
+main = defaultMain $ testGroup "Tests" [qchecks, encode_cases]
